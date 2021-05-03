e8eea6f406141fd01bbc483c263b36df45c0f9b0
Marc Raphael <marcraphael979@gmail.com>
2021-05-03 01:04:49 +0100

    contains cesar's cipher

---
+++ All-rubys-projects/MRcesar-cipher.rb
@@ -0,0 +1,26 @@
+
+def caesar_cipher(text, shift)
+  for i in 0...text.length do
+    letterIndex = text[i].ord
+    
+    (a, z) = case letterIndex #Upper and Lower cases letters
+             when 97..122 then [97, 122]   # a-z
+             when 65..90  then [65, 90]    # A-Z
+             else next
+    end
+
+    swicht = shift > 0 ? 26 : -26
+
+    letterIndex += shift
+    letterIndex -= swicht unless letterIndex.between?(a, z)
+
+    text[i] = letterIndex.chr
+  end
+end
+puts "type your text to see the result"
+message = gets.chomp
+puts "now type the shift to see (number only)"
+n_shift = gets.chomp.to_i
+caesar_cipher(message, n_shift)
+
+puts "your encrypt message is: '#{message}'✌🏾✌🏾✌🏾  "
