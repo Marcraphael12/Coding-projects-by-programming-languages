395a3dde1429ae294ffe57e6ce5c03f1c88fd39c
Marc Raphael <marcraphael979@gmail.com>
2021-07-02 10:54:13 +0100

    add linter, complete file

---
+++ All-JS-projects/book/book.js
@@ -0,0 +1,13 @@
+function Book (title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+
+  this.infos = function () {
+    return ('The ' + title + ' by ' + author + ', ' + pages + ' pages ' + read)
+  }
+}
+
+const reader = new Book('Birds', 'Marc', 100, 'already readed')
+console.log(reader.infos())
