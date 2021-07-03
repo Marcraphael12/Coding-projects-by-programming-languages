d395af6769fd9155b47bf0a1d402d33640411487
Marc Raphael <marcraphael979@gmail.com>
2021-07-03 21:28:57 +0100

    fix errors

--- All-JS-projects/Library/book.js
+++ All-JS-projects/Library/book.js
@@ -9,15 +9,15 @@ function Book (title, author, pages, read) {
 }
 
 const reader = new Book('Birds', 'Marc', 100, 'already readed')
-console.log(reader.infos());
+console.log(reader.infos())
 
-const button = document.querySelector('.btn');
-button.style.width = 'min-content';
-button.style.height = 'auto';
-button.style.background = 'unset';
-button.style.fontSize = '80px';
-button.style.fontWeight = '500';
-button.style.padding = '0 0';
-button.style.margin = '50px auto auto 50px';
-button.style.color = 'white';
-button.style.border = 'none';
+const button = document.querySelector('.btn')
+button.style.width = 'min-content'
+button.style.height = 'auto'
+button.style.background = 'unset'
+button.style.fontSize = '80px'
+button.style.fontWeight = '500'
+button.style.padding = '0 0'
+button.style.margin = '50px auto auto 50px'
+button.style.color = 'white'
+button.style.border = 'none'
