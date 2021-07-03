721435652526b9e54f7aa960c05318168931e48f
Marc Raphael <marcraphael979@gmail.com>
2021-07-03 21:22:59 +0100

    complete the html and css

--- All-JS-projects/Library/book.js
+++ All-JS-projects/Library/book.js
@@ -1,19 +1,23 @@
-let myLibrary = [];
-
 function Book (title, author, pages, read) {
   this.title = title
   this.author = author
   this.pages = pages
   this.read = read
-
   this.infos = function () {
-    return ('The ' + title + ' by ' + author + ', ' + pages + ' pages ' + read)
+    return ('The ' + title + ' by ' + author + ', ' + pages + ' pages, ' + read)
   }
 }
 
 const reader = new Book('Birds', 'Marc', 100, 'already readed')
-console.log(reader.infos())
+console.log(reader.infos());
 
-function addBookToLibrary() {
-  
-}
+const button = document.querySelector('.btn');
+button.style.width = 'min-content';
+button.style.height = 'auto';
+button.style.background = 'unset';
+button.style.fontSize = '80px';
+button.style.fontWeight = '500';
+button.style.padding = '0 0';
+button.style.margin = '50px auto auto 50px';
+button.style.color = 'white';
+button.style.border = 'none';
