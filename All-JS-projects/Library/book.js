7071b82463e784effc185d21cd456da9e934cb82
Marc Raphael <marcraphael979@gmail.com>
2021-07-05 16:57:33 +0100

    complete the html and css layout

--- All-JS-projects/Library/book.js
+++ All-JS-projects/Library/book.js
@@ -11,13 +11,3 @@ function Book (title, author, pages, read) {
 const reader = new Book('Birds', 'Marc', 100, 'already readed')
 console.log(reader.infos())
 
-const button = document.querySelector('.btn')
-button.style.width = 'min-content'
-button.style.height = 'auto'
-button.style.background = 'unset'
-button.style.fontSize = '80px'
-button.style.fontWeight = '500'
-button.style.padding = '0 0'
-button.style.margin = '50px auto auto 50px'
-button.style.color = 'white'
-button.style.border = 'none'
