c144aa05b84db1dff8dcfbdf031494c367d11d98
Marc Raphael <marcraphael979@gmail.com>
2021-07-06 02:00:24 +0100

    add updates

--- All-JS-projects/Library/book.js
+++ All-JS-projects/Library/book.js
@@ -1,13 +1,26 @@
-function Book (title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
-  this.infos = function () {
-    return ('The ' + title + ' by ' + author + ', ' + pages + ' pages, ' + read)
-  }
+
+function Library (title, author, pages, read) {
+  this.title = title || 'Book title',
+  this.author = author || 'author name',
+  this.pages = pages || 'Number of pages',
+  this.read = read || 'Readed or not?'
 }
 
-const reader = new Book('Birds', 'Marc', 100, 'already readed')
-console.log(reader.infos())
+let lbr = new Library()
+
+// the form element
+const form = document.createElement('form')
+
+// all form's inputs
+let ttl = lbr.title = document.createElement('input')
+let ath = lbr.author = document.createElement('input')
+let pgs = lbr.pages = document.createElement('input')
+let rd = lbr.read = document.createElement('input')
+
+// add inputs and styling form
+form.appendChild(ttl, ath, pgs, rd)
+form.style.display = 'flex';
+form.style.flexDirection = 'column';
+form.style.width = '100px';
+form.style.height = '250px';
 
