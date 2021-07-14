55dd5933c909d95f50a3456bed7293278b078d97
Marc Raphael <marcraphael979@gmail.com>
2021-07-14 16:50:26 +0100

    new updates

--- All-JS-projects/Library/book.js
+++ All-JS-projects/Library/book.js
@@ -1,4 +1,18 @@
 
+const div = document.createElement('div')
+div.setAttribute('style', 'width: 100%; height: auto; box-schadow: 0 0 2px blue; display: flex; justify-content: space-between;')
+
+const btn = document.querySelector('.btn')
+
+let add = document.createElement('button')
+add.setAttribute('style', 'height: auto; width: max-content; background: rgba(66, 255, 0, 1); font-weight: 800; font-size: 20px; border: none; border-radius: 5px;')
+add.innerHTML = 'add +'
+
+let cancel = document.createElement('button')
+cancel.setAttribute('style', 'height: auto; width: max-content; background: #FFF500; font-weight: 800; font-size: 20px; border: none; border-radius: 5px;')
+cancel.innerHTML = 'cancel'
+
+
 function Library (title, author, pages, read) {
   this.title = title || 'Book title',
   this.author = author || 'author name',
@@ -8,26 +22,37 @@ function Library (title, author, pages, read) {
 
 const lbr = new Library()
 
-// the form element
-const form = document.createElement('form')
+function open() {
+  // the form element
+  const form = document.createElement('form')
+  
+  // all form's inputs
+  const ttl = lbr.title = document.createElement('input')
+  ttl.style.width = '50%'
 
-// all form's inputs
-const ttl = lbr.title = document.createElement('input')
-const ath = lbr.author = document.createElement('input')
-const pgs = lbr.pages = document.createElement('input')
-pgs.setAttribute('type', 'number')
-const rd = lbr.read = document.createElement('input')
-rd.setAttribute('type', 'range')
+  const ath = lbr.author = document.createElement('input')
+  ath.style.width = '50%'
 
-const btn = document.querySelector('.btn')
+  const pgs = lbr.pages = document.createElement('input')
+  pgs.setAttribute('type', 'number')
+  pgs.setAttribute('min', '1')
+  pgs.style.width = '50%'
 
-// add inputs and styling form
-form.appendChild(ttl)
-form.appendChild(ath)
-form.appendChild(pgs)
-form.appendChild(rd)
-form.style.display = 'flex'
-form.style.flexDirection = 'column'
-form.style.width = '200px'
-form.style.height = '150px'
-document.getElementById('main').insertBefore(form, btn)
+  const rd = lbr.read = document.createElement('input')
+  rd.setAttribute('type', 'range')
+  rd.style.width = '50%'
+  
+  div.appendChild(cancel)
+  div.appendChild(add)
+  // add inputs and styling form
+  form.appendChild(ttl)
+  form.appendChild(ath)
+  form.appendChild(pgs)
+  form.appendChild(rd)
+  form.appendChild(div)
+  form.setAttribute('style', 'display: flex; flex-direction: column; width: 200px; height: 250px; box-shadow: 0 0 5px red;\
+  justify-content: space-between; position: absolute; background: black; align-items: flex-end;')
+  document.getElementById('main').insertBefore(form, btn)
+
+}
+btn.addEventListener('click', open)
